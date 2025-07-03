26f9594a99d3ba68922fb0feda8dc2264ec6f3bd
Atharva Deshmukh <atharvadeshmukh2103@gmail.com>
2025-07-03 16:01:27 +0530

    basic spec

--- tests/1.BasicTest.spec.ts
+++ tests/1.BasicTest.spec.ts
@@ -4,19 +4,18 @@ import { test, expect } from '@playwright/test';
 
  We have {page} fixture passed in every test's callback function.*/
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('Verify page title', async ({ page }) => {
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  const url: string = 'https://playwright.dev/';
+  const expectedPageTitle: string = 'Fast and reliable end-to-end testing for modern web apps | Playwright';
+
+  await page.goto(url);
+
+  const pageTitle: string = await page.title();
+
+  await expect(page).toHaveTitle(expectedPageTitle);
+
+  await page.close();
+  console.log(`Test passed: Page title is "${pageTitle}" as expected.`);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
