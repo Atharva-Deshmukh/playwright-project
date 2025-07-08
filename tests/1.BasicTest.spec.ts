3e57f43a9649284a6e08ee175218cc20ae4ea4d8
Atharva Deshmukh <atharvadeshmukh2103@gmail.com>
2025-07-08 20:39:43 +0530

    locating multiple locators

--- tests/1.BasicTest.spec.ts
+++ tests/1.BasicTest.spec.ts
@@ -19,3 +19,40 @@ test('Verify page title', async ({ page }) => {
   console.log(`Test passed: Page title is "${pageTitle}" as expected.`);
 });
 
+test('Locator with multiple matches', async ({ page }) => {
+
+  const url: string = 'https://practice-automation.com/tables/';
+  const expectedPageTitle: string = 'Tables | Practice Automation';
+  const tableCellLocator = await page.locator('.wp-block-table td');
+
+  await page.goto(url);
+  await expect(page).toHaveTitle(expectedPageTitle);
+  await expect(tableCellLocator.first()).toBeVisible();
+
+  console.log('LOCATOR ARRAY -> ', await tableCellLocator.all());
+
+  /* Outputs   LOCATOR ARRAY ->  [
+  locator('.wp-block-table td').first(),
+  locator('.wp-block-table td').nth(1),
+  locator('.wp-block-table td').nth(2),
+  locator('.wp-block-table td').nth(3),
+  locator('.wp-block-table td').nth(4),
+  locator('.wp-block-table td').nth(5),
+  locator('.wp-block-table td').nth(6),
+  locator('.wp-block-table td').nth(7)
+]*/
+});
+
+test('Extract text from locator', async ({ page }) => {
+
+  const url: string = 'https://practice-automation.com/tables/';
+  const expectedPageTitle: string = 'Tables | Practice Automation';
+  const tableCellLocator = await page.locator('.wp-block-table td');
+
+  await page.goto(url);
+  await expect(page).toHaveTitle(expectedPageTitle);
+  await expect(tableCellLocator.first()).toBeVisible();
+
+  expect(await tableCellLocator.first().innerText()).toBe('Item');
+});
+
