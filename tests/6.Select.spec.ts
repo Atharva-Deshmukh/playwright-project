3c26a34c66e098c0a6dc163c60ed66319af01d7d
Atharva Deshmukh <atharvadeshmukh2103@gmail.com>
2025-07-08 21:07:31 +0530

    select

---
+++ tests/6.Select.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+
+
+test('Verify Select', async ({ page }) => {
+
+  const url: string = 'https://practice-automation.com/form-fields/';
+  const expectedPageTitle: string = 'Form Fields | Practice Automation';
+  const selectLocator = page.locator('#automation');
+
+  await page.goto(url);
+
+  await expect(page).toHaveTitle(expectedPageTitle);
+  await expect(selectLocator).toBeVisible();
+
+  // Single selection matching the value or label
+  await selectLocator.scrollIntoViewIfNeeded();    /* For scrolling */
+  await selectLocator.selectOption('Yes');
+
+  // Single selection matching the label
+  await selectLocator.scrollIntoViewIfNeeded();
+  await selectLocator.selectOption({ label: 'No' });
+
+  // Multiple selected items
+  await selectLocator.scrollIntoViewIfNeeded();
+  await selectLocator.selectOption(['Undecided']);
+});
