8d6e3393f26814f9851ffdb40c8f6866ff612d1c
Atharva Deshmukh <atharvadeshmukh2103@gmail.com>
2025-07-15 20:45:26 +0530

    changing color config

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,6 +31,7 @@ export default defineConfig({
 
     // Collect trace for every test (enables time travel in UI)
     trace: 'on',
+    colorScheme: 'dark' /* Dark mode */
   },
 
   /* Configure projects for major browsers */
